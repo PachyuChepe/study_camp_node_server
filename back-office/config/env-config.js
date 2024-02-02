a045801cd3d8da0483f90dbb483510cac80f746f
PachyuChepe <pachyuchepe@gmail.com>
2024-02-02 10:56:26 +0900

    Fix: 백오피스 config 배포사이트로 변경

--- back-office/config/env-config.js
+++ back-office/config/env-config.js
@@ -1,4 +1,4 @@
-// const SOCKET = 'https://www.big1-express.store';
-const SOCKET = 'http://localhost:3500';
+const SOCKET = 'https://www.big1-express.store';
+// const SOCKET = 'http://localhost:3500';
 
 export { SOCKET };
