1746747d5633e55fa865968df8aa05daa047602c
PachyuChepe <pachyuchepe@gmail.com>
2024-02-09 11:40:05 +0900

    Feat: 로드밸런싱 서버간 소켓 동기화

--- src/redis/redisClient.js
+++ src/redis/redisClient.js
@@ -1,10 +1,11 @@
 // redisClient.js
-import env from './env.config.js';
 import redis from 'redis';
+// import { configDotenv } from 'dotenv';
+// configDotenv();
 
 // Redis 클라이언트 인스턴스 생성
 const redisClient = redis.createClient({
-  url: `redis://${env.REDIS_USERNAME}:${env.REDIS_PASSWORD}@${env.REDIS_HOST}:${env.REDIS_PORT}/0`,
+  url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
 });
 
 // Redis 에러 핸들링을 위한 이벤트 리스너 설정
